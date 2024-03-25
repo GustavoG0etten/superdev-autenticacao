8349d76104df16b48ba9cac50a11d94034f1fdf3
GustavoG0etten <gustavogoetten87@gmail.com>
2024-03-25 20:32:30 -0300

    Criando metodos GETTERs e SETTERs

---
+++ src/br/com/projetoAutenticacao/model/Usuario.java
@@ -0,0 +1,79 @@
+package br.com.projetoAutenticacao.model;
+
+public class Usuario {
+	
+	private int id;
+	private String nome;
+	private String login;
+	private String senha;
+	private boolean isAtivo;
+	
+	
+	public Usuario() {
+		
+	}
+	
+	public Usuario(int id, String nome, String login, String senha, boolean isAtivo) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.login = login;
+		this.senha = senha;
+		this.isAtivo = isAtivo;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public String getLogin() {
+		return login;
+	}
+
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+
+	public String getSenha() {
+		return senha;
+	}
+
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+
+	public boolean isAtivo() {
+		return isAtivo;
+	}
+
+
+	public void setAtivo(boolean isAtivo) {
+		this.isAtivo = isAtivo;
+	}
+	
+	
+	
+	
+
+}
